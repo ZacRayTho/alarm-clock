bc4f89aa0d0bef802d1db8a24e014cfaefae8988
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-02-02 17:16:23 +0000

    Alarm mvp

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const clock = document.getElementById("clock");
+const alarm = document.getElementById("alarm");
+const submit = document.getElementById("submit");
+const set = document.getElementById("set");
 let test = new Date();
 
 function time () {
@@ -11,3 +14,12 @@ function time () {
 setInterval(function () {test = new Date()}, 1000);
 setInterval(function () {clock.innerHTML = time()}, 1000);
 
+submit.addEventListener("click", ()=>{
+    set.innerHTML= alarm.value;
+})
+
+setInterval(function () {
+    if (clock.innerHTML == set.innerHTML + ":0"){
+        alert("ALARM")
+    }
+}, 1000);
